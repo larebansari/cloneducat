9e07fc252f0118db5fdb37e526ae78b2c692caab
larebansari <ansarilareb00@gmail.com>
2023-11-18 11:14:14 +0530

    clone ducat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Mylandingpage from './modals/components/Mylandingpage';
+import Myabout from './modals/components/Myabout';
+import Mycertificate from './modals/components/Mycertificat'
+import Myplacement from './modals/components/Myplacement'
+import Myblog from './modals/components/Myblog'
+import Mycontact from './modals/components/Mycontact'
+import Myfooter from './modals/share/Myfooter';
+import Mycourses from './modals/components/Mycourses';
+import Myjava from './modals/components/subcomponent/Myjava';
+import Mynavbar from './modals/share/Mynavbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BrowserRouter>
+    <Mynavbar/>
+    <Routes>
+      <Route path='/' element={<Mylandingpage/>}/>
+      <Route path='about' element={<Myabout/>}/>
+      <Route path='certificate' element={<Mycertificate/>}/>
+      <Route path='placement' element={<Myplacement/>}/>
+      <Route path='blog' element={<Myblog/>}/>
+      <Route path='contact' element={<Mycontact/>}/>
+      <Route path='courses' element={<Mycourses/>}>
+        <Route path='couses/java' element={<Myjava/>}/>
+      </Route>
+      
+    </Routes>
+    <Myfooter/>
+    </BrowserRouter>
+      
+    </>
+  )
 }
 
-export default App;
+export default App
